7341c47509ab9fd9c262945fed7db5ac102f8207
Nagendrarana <boysmarty693@gmail.com>
2025-03-30 07:12:07 +0530

    Added a state variable using useState hook

--- react/my-react-app/src/components/button/button.jsx
+++ react/my-react-app/src/components/button/button.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function Button({text}) {
+    let [x, setX] = useState(0);
     return (
-      <button className="px-4 py-2 bg-blue-500 text-white rounded">
+      <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={()=>{
+        setX(x+1);
+      }}>
        {text}
+       {x}
       </button>
     );
   }

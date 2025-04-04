d437ef923983e982ecba93190a7b0451e4e3eb09
Nagendrarana <boysmarty693@gmail.com>
2025-04-04 23:05:08 +0530

    Added routes

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,18 +1,45 @@
-
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
 import Button from './components/button/button'
 import TextInputForm from './components/TextInputForm/TextInputForm'
 
-function App() {
- 
-
+function Home() {
   return (
-  <>
-  <h1>HangMan</h1>
-  <TextInputForm />
-  <Button text="Click me" />
-  </>
-  
+    <div>
+      <h1>HangMan</h1>
+      <TextInputForm />
+      <Button text="Click me" />
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the HangMan game made in React.</p>
+    </div>
+  )
+}
+
+function NotFound() {
+  return <h1>404 - Page Not Found</h1>
+}
+
+function App() {
+  return (
+    <>
+      <nav style={{ padding: '10px', backgroundColor: '#eee' }}>
+        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 

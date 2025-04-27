3a45fdfcc898ed51b730088d0b8337698697edaa
Nagendrarana <boysmarty693@gmail.com>
2025-04-27 17:45:46 +0530

    Added useQuery

--- react/my-react-app/src/components/CoinTable/CoinTable.jsx
+++ react/my-react-app/src/components/CoinTable/CoinTable.jsx
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 import { fetchCoinData } from "../../services/fetchCoinData.js";
-
+import { useQuery} from "react-query";
 function CoinTable(){
     async function Download(){
         const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
         const data = await response.json();
         console.log(data);
     }
-    useEffect(
-       () =>{
-        // Download()
-        fetchCoinData();
-       }
-        ,[]
-    )
+    const { data, isLoading, error } = useQuery("coins", fetchCoinData);
+
+       console.log(data);
+       console.log(isLoading);
+       console.log(error)
+
+  
     return(
         <div>
             CoinTable
